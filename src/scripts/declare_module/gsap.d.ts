c8ee934e720023cc7a16403495d833341edfaff3
Zedan Saheer <zed.saheer5@gmail.com>
2023-06-23 00:35:46 +0300

    Generated build file, Completed animation flow upto 70%

--- src/scripts/declare_module/gsap.d.ts
+++
@@ -1,4 +0,0 @@
-// .d.ts file
-declare module "https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.0/gsap.min.js" {
-    export const gsap : any;
-} 
--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -5,15 +5,67 @@ let id = 0;
 let counter = document.getElementById("counter");
 let loadTimeline = gsap.timeline();
 
-loadTimeline.from('.percentage',{
- duration:1,
- opacity:0,
+loadTimeline.from('.percentage', {
+    duration: 2,
+    opacity: 0,
 });
 
-loadTimeline.from('.counter',{
- duration:2,
- opacity:0,
+loadTimeline.from('.counter', {
+    duration: 2,
+    opacity: 0,
+}, "<");
+
+loadTimeline.to('.counter', {
+    duration: 4,
+    opacity: 0,
 });
+
+loadTimeline.to('.percentage', {
+    duration: 4,
+    opacity: 0,
+}, "<");
+
+gsap.to(".mt_text",{
+    xPercent: 100,
+    duration: 8,
+    repeat: -1, 
+});
+gsap.to(".mb_text",{
+    xPercent: -100,
+    duration: 8,
+    repeat: -1, 
+});
+
+loadTimeline.from(".top", {
+    yPercent: -100,
+    duration: 2.5, ease: "power4.out",
+}, 2.5);
+
+loadTimeline.from(".bottom", {
+    yPercent: 100,
+    duration: 2.5, ease: "power4.out",
+}, 2.5);
+
+loadTimeline.from(".first_text", {
+    opacity:0,
+    y:100,
+});
+
+loadTimeline.to(".first_text", {
+    opacity:1,
+    x:-200,
+});
+
+loadTimeline.from(".second_text",{
+    opacity:0,
+    y:100,
+});
+
+loadTimeline.to(".second_text",{
+    opacity:1,
+    x:200,
+});
+
 
 
 /**
@@ -33,7 +85,7 @@ function frame() {
         loadTimeline.play();
     } else {
         id++;
-        if (counter!) counter.innerText = id.toString();
+        if (counter) counter.innerText = id.toString();
     }
 };
 
