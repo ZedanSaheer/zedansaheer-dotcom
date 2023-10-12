b6694c3bbd9a6eb6511132f689dad6756acb8f8d
Zedan Saheer <zed.saheer5@gmail.com>
2023-10-12 17:41:14 +0530

    Removed mdx integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
-import mdx from "@astrojs/mdx";
-
-// https://astro.build/config
 export default defineConfig({
   site: 'https://zedansaheer.com',
-  integrations: [tailwind(), sitemap(), mdx()]
+  integrations: [tailwind(), sitemap()]
 });
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,7 +3,7 @@ import rss from '@astrojs/rss';
 export async function GET(context) {
     return rss({
         title: 'Zedan Saheer',
-        description: '',
+        description: 'Zedan Saheer is a Software Engineer, Innovator, Content Writer and an Indie Chef. Focused on building highly performant and scalable code.',
         site: context.site,
         items: [],
         customData: `<language>en-us</language>`,
