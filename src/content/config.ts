2386116dcab99f678370d536bc64edabb42522b0
Zedan Saheer <zed.saheer5@gmail.com>
2023-10-19 23:32:12 +0530

    Fixed minor bugs for load performance in blogs

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,14 +1,17 @@
 import { z, defineCollection } from "astro:content";
-import type { ImageMetadata } from "astro";
+
+const keywordArray = z.enum(['Software Engineering', 'JavaScript', 'TypeScript', 'Web Development', 'Life', 'Thoughts', 'Wisdom', 'Motivation']);
+
+const publishDateTransformedToLocaleDate = z.string().transform((str) => new Date(str).toLocaleDateString('en-us', { weekday: "long", year: "2-digit", month: "2-digit", day: "2-digit" }));
 
 const blogCollection = defineCollection({
   type: 'content',
   schema: ({ image }) => z.object({
     title: z.string(),
     description: z.string(),
-    tags: z.array(z.enum(['Software Engineering', 'JavaScript', 'TypeScript', 'Web Development', 'Life', 'Thoughts', 'Wisdom', 'Motivation'])),
+    tags: z.array(keywordArray),
     author: z.enum(['Zedan Saheer']),
-    publishDate: z.string().transform((str) => new Date(str).toLocaleDateString('en-us', { weekday: "long", year: "2-digit", month: "2-digit", day: "2-digit" })),
+    publishDate: publishDateTransformedToLocaleDate,
     image: z.object({
       alt: z.string(),
       src: image(),
