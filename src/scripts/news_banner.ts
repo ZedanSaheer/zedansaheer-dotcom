12ecc6c8ab42039f842ba86a85cda04922479978
Zedan Saheer <zed.saheer5@gmail.com>
2023-10-27 19:24:06 +0530

    Added news banner for important updates

---
+++ src/scripts/news_banner.ts
@@ -0,0 +1,9 @@
+const container = document.querySelector(".news-container");
+const data = document.querySelector(".news-data");
+
+console.log(container.clientWidth);
+console.log(data.clientWidth);
+
+if (container.clientWidth < data.clientWidth) {
+    data.classList.add("animate");
+}
