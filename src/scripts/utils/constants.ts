3f47f069ef44c1fc3b82ff31bbd4cde5f591034f
ZedanSaheer <zed.saheer5@gmail.com>
2023-10-30 13:04:23 +0530

    Added new section for home, Refactored blob pattern in hero section

--- src/scripts/utils/constants.ts
+++ src/scripts/utils/constants.ts
@@ -24,7 +24,7 @@ export interface Header {
     title?: string;
     description?: string;
     image: {
-        alt: string;
+        alt: string | undefined;
         src: string;
         share: string;
         shareDark: string;
